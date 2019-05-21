cb3783db3348e8a2bb4637a6889721cda56491a5
Nick Playfair <nick@chaosemerald.co.uk>
2019-05-21 15:28:18 +0100

    Install koa router and ejs

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-const Koa = require('koa');
+const Koa       = require('koa');
+const KoaRouter = require('koa-router');
+const json      = require('koa-json');
+const path      = require('path');
+const render    = require('koa-ejs');
 
 const app = new Koa();
+const router = new KoaRouter();
 
-app.use(async ctx => ctx.body = 'Hello world');
+// JSON prettier middleware
+app.use(json());
+
+// Router middleware
+app.use(router.routes()).use(router.allowedMethods());
+
+// Simple example
+// app.use(async ctx => ctx.body = { msg: 'Hello world' });
+
+router.get('/test', ctx => (ctx.body = 'Test'));
+
 
 app.listen(3000, () => console.log('server started'));
